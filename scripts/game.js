a0a1cecb2d78a9e9ae8c50939164411050ee3262
oraowycliffe@students.uonbi.ac.ke <oraowycliffe@students.uonbi.ac.ke>
2022-07-19 19:34:35 +0300

    Initialize turn control and dataset follow up

--- scripts/game.js
+++ scripts/game.js
@@ -1,23 +1,36 @@
-function startNewGame(){
-    if(players[0].name  === '' || players[1].name === '' ){
-        alert('Please set custom names for the players')
-        return;
-    }
-    activePlayerNameElement.textContent = players[activePlayer].name;
-    gameAreaElement.style.display = 'block';
+function startNewGame() {
+  if (players[0].name === "" || players[1].name === "") {
+    alert("Please set custom names for the players");
+    return;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = "block";
 }
 
-function switchPlayer(){
-    if(activePlayer === 0){
-        activePlayer = 1;
-    }else{
-        activePlayer = 0;
-    }
-    activePlayerNameElement.textContent = players[activePlayer].name;
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
 }
 
-function selectGameField(event){
-    event.target.textContent = players[activePlayer].Symbol;
-    event.target.classList.add('disabled');
-    switchPlayer();
+function selectGameField(event) {
+  const selectedField = event.target;
+
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert("Oops select a new spot");
+    return;
+  }
+
+  event.target.textContent = players[activePlayer].Symbol;
+  event.target.classList.add("disabled");
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+  console.log(gameData);
+  switchPlayer();
 }
