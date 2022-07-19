0f5102f56503918e37279d637ca880ce7571b17d
oraowycliffe@students.uonbi.ac.ke <oraowycliffe@students.uonbi.ac.ke>
2022-07-19 19:09:01 +0300

    Manage player turns

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,23 @@
 function startNewGame(){
     if(players[0].name  === '' || players[1].name === '' ){
-        alert('Please set custom for the players')
+        alert('Please set custom names for the players')
         return;
     }
+    activePlayerNameElement.textContent = players[activePlayer].name;
     gameAreaElement.style.display = 'block';
+}
+
+function switchPlayer(){
+    if(activePlayer === 0){
+        activePlayer = 1;
+    }else{
+        activePlayer = 0;
+    }
+    activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event){
+    event.target.textContent = players[activePlayer].Symbol;
+    event.target.classList.add('disabled');
+    switchPlayer();
 }
