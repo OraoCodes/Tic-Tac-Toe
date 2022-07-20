33304e898da744b328e3ce79a902ced3cc0ced61
oraowycliffe@students.uonbi.ac.ke <oraowycliffe@students.uonbi.ac.ke>
2022-07-20 19:01:06 +0300

    Game reset v1

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,30 @@
+function resetGameStatus(){
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML = '<span id="winner-name">PLAYER NAME</span> Wins!';
+  gameOverElement.style.display = 'none';
+
+  let gameBoardIndex = 0;
+
+  for (let i; i < 3; i++){
+    for (let j; j < 3; j++){
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = '';
+      gameBoardItemElement.classList.remove('disabled');
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
   if (players[0].name === "" || players[1].name === "") {
     alert("Please set custom names for the players");
     return;
   }
+  resetGameStatus();
+
   activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = "block";
 }
@@ -22,7 +44,7 @@ function selectGameField(event) {
   const selectedColumn = selectedField.dataset.col - 1;
   const selectedRow = selectedField.dataset.row - 1;
 
-  if (gameData[selectedRow][selectedColumn] > 0) {
+  if (gameData[selectedRow][selectedColumn] > 0 || gameIsOver )  {
     alert("Oops select a new spot");
     return;
   }
@@ -33,48 +55,63 @@ function selectGameField(event) {
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
   console.log(gameData);
   const winnerId = checkForGameOver();
-  console.log(winnerId);
-  
-  currentRound ++;
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
+
+  currentRound++;
   switchPlayer();
 }
 
-function checkForGameOver(){
-    for (let i; i < 3; i++){
-        if (
-            gameData[i][0] > 0 &&
-            gameData[i][0] === gameData[i][1] && 
-            gameData[i][1] === gameData[i][2]
-        ){
-        return gameData[i][0];
-        }
-    }
-    for (let i; i < 3; i++){
-        if (
-            gameData[0][i] > 0 &&
-            gameData[0][i] === gameData[1][i] && 
-            gameData[0][i] === gameData[2][i]
-        ){
-        return gameData[0][i];
-        }
-    }
-    //diagonal check
+function checkForGameOver() {
+  for (let i; i < 3; i++) {
     if (
-        gameData[0][0] > 0 &&
-        gameData[0][0] === gameData[1][1] && 
-        gameData[1][1] === gameData[2][2]
-    ){
-        return gameData[0][0];
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
     }
+  }
+  for (let i; i < 3; i++) {
     if (
-        gameData[2][0] > 0 &&
-        gameData[2][0] === gameData[1][1] && 
-        gameData[1][1] === gameData[0][2]
-    ){
-        return gameData[2][0];
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
     }
-    if( currentRound === 9){
-        return -1;
-    }
-    return 0;
+  }
+  //diagonal checks
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+  if (currentRound === 9) {
+    return -1;
+  }
+  return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = "block";
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "A draw!";
+  }
 }
